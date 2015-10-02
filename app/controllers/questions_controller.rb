1c7ce2734358b69ff8047758eda217ffcaa5c08c
Domingo Moronta <email": null>
2015-10-02 19:20:14 -0400

    fixing gemfile dependencies

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -65,7 +65,7 @@ class QuestionsController < ApplicationController
   private
 
   def question_params
-    params.require(:question).permit(:title), :content)
+    params.require(:question).permit(:text)
   end
 
   def find_question
