131ea9fb54c6d8f9f4caeb130bb8e60773036ed4
Julianna Rusakiewicz <Juliannarusak@gmail.com>
2015-10-02 17:42:41 -0400

    Add test for answers create

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -7,4 +7,20 @@ class AnswersController < ApplicationController
   def show
     @answer = Answer.find params[:id]
   end
+
+  def create
+    @answer = Answer.new answers_params
+    if @answer.save
+      redirect_to answer(@answer)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def answers_params
+    params.require(:answer).permit(:text)
+  end
+
 end
