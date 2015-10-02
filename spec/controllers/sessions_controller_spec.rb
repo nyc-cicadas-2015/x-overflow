7d5fc36f45f212edf521e02f47344cf2b8802741
Dian Pan <dian.di.pan@gmail.com>
2015-10-02 19:53:01 -0400

    add unsuccessful tests and code

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -35,7 +35,15 @@ describe SessionsController do
       expect(response).to redirect_to root_path
     end
 
+    it 'sets a flash error message' do
+      post :create, session: { username: nil, password: nil }
+      expect(flash[:error]).to have_content 'Incorrect username or password'
+    end
 
+    it 'does not set the user session' do
+      post :create, session: { username: nil, password: nil }
+      expect(session[:user_id]).to be_nil
+    end
   end
 
 end
