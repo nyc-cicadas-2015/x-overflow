2ad3cce7a119e3f262ab7d4c179c6d6b1f0f3048
Dian Pan <dian.di.pan@gmail.com>
2015-10-02 16:50:59 -0400

    add bcrypt, add user auth and validations in table, add controller user new test, test passes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  validates_uniqueness_of :username
+
   has_many :questions
   has_many :answers
   has_many :comments
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    
+    username Faker::Internet.email
+    password Faker::Internet.password(8)
   end
 
 end
