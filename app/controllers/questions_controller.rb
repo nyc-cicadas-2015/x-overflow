8c0b1806cd3b0a23c41ce0a8e34489e2ef6fdd6e
Domingo Moronta <email": null>
2015-10-02 19:04:58 -0400

    add show method to the questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,6 +7,30 @@ class QuestionsController < ApplicationController
     @questions = Question.order_by_recent
   end
 
+# def trending
+#     @question = Question.order_by_trending
+#     render :index
+#   end
+
+#   def votes
+#     @questions = Question.order_by_votes
+#     render :index
+#   end
+
+#   def search
+#     @questions = Question.where("title ILOOK ?", "%#{params[:search]}%")
+#     @questions.order_by_recent
+#     render :index
+#   end
+
+  def show
+    @comments = @questions.comments
+    @answers = @question.answers.includes(:comments)
+    @vote = Vote.new
+    @answer = Answer.new
+    @comment = Comment.new
+  end
+
 
 
 end
