a8c4a0d1e37983a380abc4ec59b0ce1d72daab67
Domingo Moronta <email": null>
2015-10-03 16:20:54 -0400

    finishing the views to make tests pass

--- db/migrate/20151002183439_create_questions.rb
+++ db/migrate/20151002183439_create_questions.rb
@@ -3,6 +3,7 @@ class CreateQuestions < ActiveRecord::Migration
     create_table :questions do |t|
       t.references :user, index: true, null:false
       t.string :text, null:false
+      t.string :title,null:false
 
       t.timestamps null:false
     end
