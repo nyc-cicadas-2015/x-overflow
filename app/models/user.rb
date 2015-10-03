f6913f9aad6f9b3e0583d2c0e94441d1b67ded8a
Dian Pan <dian.di.pan@gmail.com>
2015-10-03 15:27:00 -0400

    add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates_uniqueness_of :username
+  validates_presence_of :username
   validates :password, :presence => true, :length => { minimum: 6 }, :on => :create
-  validates :username, length: { maximum: 50 }
 
   has_many :questions
   has_many :answers
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'validates uniqueness of username' do
+    User.create!(:username=>"Foo", :password=>"test123")
+    foo = User.new(:username=>"Foo", :password=>"123test")
+    foo.should_not be_valid
+    foo.errors[:username].should include("has already been taken")
+  end
+
+  it 'validates that password is at least 6 characters long' do
+    foo = User.new(:username=>"Foo", :password=>"test")
+    foo.should_not be_valid
+  end
+
 end
