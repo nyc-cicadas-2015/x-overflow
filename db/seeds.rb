3c0f924f8d7938c50007b2efcf20a33dcaa24eb8
Dian Pan <dian.di.pan@gmail.com>
2015-10-03 17:52:27 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+dian = User.create(username: "dianpan", password: "test123")
+julianna = User.create(username: "queenB", password: "abc123")
+domingo = User.create(username: "dj-m", password: "yoyoyo123")
+
+10.times do
+  new_user = User.create(username: Faker::Internet.user_name, password: "securepassword")
+  5.times do
+    new_user.questions.create(text: Faker::Hacker.say_something_smart)
+  end
+end
 
 
-dian = User.create(username: "dianpan", password: )
+q1 = dian.questions.create(text: "Why am I not a mutant and how can I become one?")
+q2 = julianna.questions.create(text: "How can I help all the mutant children?")
+q3 = domingo.questions.create(text: "How can I get Storm to notice me?")
+q4 = dian.questions.create(text: "Why can't Wolverine die?")
+q5 = julianna.questions.create(text: "Why aren't Wolverine's claws still bone in the future?")
+q6 = domingo.questions.create(text: "How did Magneto get his powers back?")
+q7 = dian.questions.create(text: "Soooo... is Mystique good or bad?")
+q8 = julianna.questions.create(text: "Does Professor X actually die?")
+q9 = domingo.questions.create(text: "How does Cyclops see anything?")
+q10 = dian.questions.create(text: "What's the big deal about Jean Grey? Why does Is the rivalry between Cyclops and Wolverine p")
