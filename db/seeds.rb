0bae83049087c7d2af5b52473c6330cc4285628a
Dian Pan <dian.di.pan@gmail.com>
2015-10-04 19:50:51 -0400

    add answers, question comments and answer comments to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,14 +21,21 @@ q9 = domingo.questions.create!(title: "How does Cyclops see anything?", text: "W
 q10 = dian.questions.create!(title: "What's the big deal about Jean Grey?", text: "Seriously what is up with those hotties, Cyclops and Wolverine fighting over her")
 
 
+a1 = q1.answers.create!(user_id: domingo.id, question_id: q1.id, text: "You need to be happy with who you are, stop wishing you were someone different")
+a2 = q1.answers.create!(user_id: domingo.id, question_id: q1.id, text: "Just popping in to say hello")
+a3 = q5.answers.create!(user_id: dian.id, question_id: q1.id, text: "That's a really great question, I never thought about it before")
+a4 = q5.answers.create!(user_id: domingo.id, question_id: q1.id, text: "It's a mystery man")
+
+
 q1.comments.create!(text: "Whoa whoa whoa", user_id: julianna.id, commentable_id: q1.id, commentable_type: "Question")
 q1.comments.create!(text: "I'm not sure but I'll add you to my secret Mutant lover group on fb", user_id: domingo.id, commentable_id: q1.id, commentable_type: "Question")
 q1.comments.create!(text: "Can you add me too?", user_id: julianna.id, commentable_id: q1.id, commentable_type: "Question")
 q3.comments.create!(text: "Try bringing her flowers!", user_id: dian.id, commentable_id: q1.id, commentable_type: "Question")
 q3.comments.create!(text: "What a silly question", user_id: julianna.id, commentable_id: q1.id, commentable_type: "Question")
 
-q1.answers.create!(user_id: domingo.id, question_id: q1.id, text: "You need to be happy with who you are, stop wishing you were someone different")
-q1.answers.create!(user_id: domingo.id, question_id: q1.id, text: "Just popping in to say hello")
-q5.answers.create!(user_id: dian.id, question_id: q1.id, text: "That's a really great question, I never thought about it before")
-q5.answers.create!(user_id: domingo.id, question_id: q1.id, text: "It's a mystery man")
 
+q1.comments.create!(text: "This is a comment", user_id: julianna.id, commentable_id: a1.id, commentable_type: "Answer")
+q1.comments.create!(text: "Here is another comment", user_id: domingo.id, commentable_id: q1.id, commentable_type: "Answer")
+q1.comments.create!(text: "yo yo yo", user_id: julianna.id, commentable_id: a2.id, commentable_type: "Answer")
+q3.comments.create!(text: "just droppin by", user_id: dian.id, commentable_id: a3.id, commentable_type: "Answer")
+q3.comments.create!(text: "idk what to say lol", user_id: julianna.id, commentable_id: a4.id, commentable_type: "Answer")
