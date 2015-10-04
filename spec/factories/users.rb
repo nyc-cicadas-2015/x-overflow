66da434fb1b48cc5ff661b4ee52a5ea310d0dd8d
dj-m <domingomoronta@gmail.com>
2015-10-04 12:17:10 -0400

    still pending tests on votes controller

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     username Faker::Internet.email
-    password Faker::Internet.password
+    password Faker::Internet.password(8)
   end
 
   factory :new_user do
