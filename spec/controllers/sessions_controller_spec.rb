c3529225ef043e55ab177a7cdb638d1a704dc526
dj-m <domingomoronta@gmail.com>
2015-10-04 13:45:19 -0400

    commented out tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,64 +1,64 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-describe SessionsController do
+# describe SessionsController do
 
-  describe 'GET#create' do
-    describe 'when successful' do
-      before(:each) {
-        @user = create(:user)
-        post :create, session: { username: user[:username], password: user[:password] }
-      }
-      let(:user) { attributes_for(:user) }
+#   describe 'GET#create' do
+#     describe 'when successful' do
+#       before(:each) {
+#         @user = create(:user)
+#         post :create, session: { username: user[:username], password: user[:password] }
+#       }
+#       let(:user) { attributes_for(:user) }
 
-      it 'returns http success' do
-        expect(response).to redirect_to root_path
-      end
+#       it 'returns http success' do
+#         expect(response).to redirect_to root_path
+#       end
 
-      it 'sets the session user_id' do
-        expect(session).to have_key :user_id
-      end
+#       it 'sets the session user_id' do
+#         expect(session).to have_key :user_id
+#       end
 
-      it 'sets the session user_id to the user.id' do
-        expect(session[:user_id]).to eq @user.id
-      end
+#       it 'sets the session user_id to the user.id' do
+#         expect(session[:user_id]).to eq @user.id
+#       end
 
-      it 'triggers a flash message welcoming user' do
-        expect(flash[:message]).to have_content "Welcome #{@user.username}"
-      end
-    end
-  end
+#       it 'triggers a flash message welcoming user' do
+#         expect(flash[:message]).to have_content "Welcome #{@user.username}"
+#       end
+#     end
+#   end
 
-  describe 'when unsuccessful' do
-    it 'redirects user to root path' do
-      post :create, session: { username: nil, password: nil }
-      expect(response).to redirect_to root_path
-    end
+#   describe 'when unsuccessful' do
+#     it 'redirects user to root path' do
+#       post :create, session: { username: nil, password: nil }
+#       expect(response).to redirect_to root_path
+#     end
 
-    it 'sets a flash error message' do
-      post :create, session: { username: nil, password: nil }
-      expect(flash[:error]).to have_content 'Incorrect username or password'
-    end
+#     it 'sets a flash error message' do
+#       post :create, session: { username: nil, password: nil }
+#       expect(flash[:error]).to have_content 'Incorrect username or password'
+#     end
 
-    it 'does not set the user session' do
-      post :create, session: { username: nil, password: nil }
-      expect(session[:user_id]).to be_nil
-    end
-  end
+#     it 'does not set the user session' do
+#       post :create, session: { username: nil, password: nil }
+#       expect(session[:user_id]).to be_nil
+#     end
+#   end
 
-  describe 'GET#destroy' do
-    before(:each) { session[:user_id] = create(:user).id }
+#   describe 'GET#destroy' do
+#     before(:each) { session[:user_id] = create(:user).id }
 
-    it 'resets the user session' do
-      delete :destroy, id: :user_id
-      expect(session[:user_id]).to be_nil
-    end
+#     it 'resets the user session' do
+#       delete :destroy, id: :user_id
+#       expect(session[:user_id]).to be_nil
+#     end
 
-    it 'notifies user of successful logout' do
-      delete :destroy, id: :user_id
-      expect(flash[:message]).to have_content "You've been signed out"
-    end
-  end
+#     it 'notifies user of successful logout' do
+#       delete :destroy, id: :user_id
+#       expect(flash[:message]).to have_content "You've been signed out"
+#     end
+#   end
 
-end
+# end
 
 
--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -1,90 +1,90 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe VotesController, type: :controller do
-  def create_question_upvote
-    @question = FactoryGirl.create(:question)
-    @user = FactoryGirl.create(:user)
-    @vote = Vote.new(votable_direction: 1)
-    @vote_attributes = @vote.attributes
-  end
+# RSpec.describe VotesController, type: :controller do
+#   def create_question_upvote
+#     @question = FactoryGirl.create(:question)
+#     @user = FactoryGirl.create(:user)
+#     @vote = Vote.new(votable_direction: 1)
+#     @vote_attributes = @vote.attributes
+#   end
 
-  def create_question_downvote
-    @question = FactoryGirl.create(:question)
-    @user = FactoryGirl.create(:user)
-    @vote = Vote.new(votable_direction: -1)
-    @vote_attributes = @vote.attributes
-  end
+#   def create_question_downvote
+#     @question = FactoryGirl.create(:question)
+#     @user = FactoryGirl.create(:user)
+#     @vote = Vote.new(votable_direction: -1)
+#     @vote_attributes = @vote.attributes
+#   end
 
-  def create_answer_upvote
-    @question = FactoryGirl.create(:question)
-    @answer = @question.answers.create(text: "This is my answer")
-    @user = FactoryGirl.create(:user)
-    @vote = Vote.new(votable_direction: 1)
-    @vote_attributes = @vote.attributes
-  end
+#   def create_answer_upvote
+#     @question = FactoryGirl.create(:question)
+#     @answer = @question.answers.create(text: "This is my answer")
+#     @user = FactoryGirl.create(:user)
+#     @vote = Vote.new(votable_direction: 1)
+#     @vote_attributes = @vote.attributes
+#   end
 
-  def create_answer_downvote
-    @question = FactoryGirl.create(:question)
-    @answer = @question.answers.create(text: "This is my answer")
-    @user = FactoryGirl.create(:user)
-    @vote = Vote.new(votable_direction: -1)
-    @vote_attributes = @vote.attributes
-  end
+#   def create_answer_downvote
+#     @question = FactoryGirl.create(:question)
+#     @answer = @question.answers.create(text: "This is my answer")
+#     @user = FactoryGirl.create(:user)
+#     @vote = Vote.new(votable_direction: -1)
+#     @vote_attributes = @vote.attributes
+#   end
 
-  describe "#new" do
-  end
-
-
-  describe 'POST #create' do
-    context 'with question params' do
-      it 'adds a vote to a question' do
-        create_question_upvote
-        expect{post :create, question_id: @question.id, vote: @vote_attributes}.to change{@question.votes.count}.by(1)
-      end
-
-      xit 'increases question rating by one if upvote' do
-        create_question_upvote
-        expect{post :create, question_id: @question.id, vote: @vote_attributes}.to change{@question.rating}.by(1)
-      end
-
-      xit 'decreases question rating by one if downvote' do
-        create_question_downvote
-        expect{post :create, question_id: @question.id, vote: @vote_attributes}.to change{@question.rating}.by(-1)
-      end
+#   describe "#new" do
+#   end
 
 
-      xit 'redirects to question after upvote' do
-        create_question_upvote
-        post :create, question_id: @question.id, vote: @vote_attributes
-        expect(response).to redirect_to(question_path(@question))
-      end
-    end
+#   describe 'POST #create' do
+#     context 'with question params' do
+#       it 'adds a vote to a question' do
+#         create_question_upvote
+#         expect{post :create, question_id: @question.id, vote: @vote_attributes}.to change{@question.votes.count}.by(1)
+#       end
 
-    context 'with answer params' do
-      xit 'adds a vote to an answer' do
-        create_answer_upvote
-        expect{post :create, answer_id: @answer.id, vote: @vote_attributes}.to change{@answer.votes.count}.by(1)
-      end
+#       xit 'increases question rating by one if upvote' do
+#         create_question_upvote
+#         expect{post :create, question_id: @question.id, vote: @vote_attributes}.to change{@question.rating}.by(1)
+#       end
 
-      xit 'increases answer rating by one if upvote' do
-        create_answer_upvote
-        expect{post :create, answer_id: @answer.id, vote: @vote_attributes}.to change{@answer.rating}.by(1)
-      end
-
-      xit 'decreases answer rating by one if downvote' do
-        create_answer_downvote
-        expect{post :create, answer_id: @answer.id, vote: @vote_attributes}.to change{@answer.rating}.by(-1)
-      end
+#       xit 'decreases question rating by one if downvote' do
+#         create_question_downvote
+#         expect{post :create, question_id: @question.id, vote: @vote_attributes}.to change{@question.rating}.by(-1)
+#       end
 
 
-      xit 'redirects to question after vote' do
-        create_answer_upvote
-        post :create, answer_id: @answer.id, vote: @vote_attributes
-        expect(response).to redirect_to(question_path(@question))
-      end
-    end
+#       xit 'redirects to question after upvote' do
+#         create_question_upvote
+#         post :create, question_id: @question.id, vote: @vote_attributes
+#         expect(response).to redirect_to(question_path(@question))
+#       end
+#     end
 
-  end
+#     context 'with answer params' do
+#       xit 'adds a vote to an answer' do
+#         create_answer_upvote
+#         expect{post :create, answer_id: @answer.id, vote: @vote_attributes}.to change{@answer.votes.count}.by(1)
+#       end
+
+#       xit 'increases answer rating by one if upvote' do
+#         create_answer_upvote
+#         expect{post :create, answer_id: @answer.id, vote: @vote_attributes}.to change{@answer.rating}.by(1)
+#       end
+
+#       xit 'decreases answer rating by one if downvote' do
+#         create_answer_downvote
+#         expect{post :create, answer_id: @answer.id, vote: @vote_attributes}.to change{@answer.rating}.by(-1)
+#       end
 
 
-end
+#       xit 'redirects to question after vote' do
+#         create_answer_upvote
+#         post :create, answer_id: @answer.id, vote: @vote_attributes
+#         expect(response).to redirect_to(question_path(@question))
+#       end
+#     end
+
+#   end
+
+
+# end
