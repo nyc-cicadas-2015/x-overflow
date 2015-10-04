e8bda35a87e07bcbbf2cc7d8157e851eb98be1e8
dj-m <domingomoronta@gmail.com>
2015-10-04 11:55:22 -0400

    modifying the votes tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     username Faker::Internet.email
-    password Faker::Internet.password(8)
+    password Faker::Internet.password
   end
 
   factory :new_user do
